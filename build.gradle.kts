3557c0d81c04a2b733a89be75d6d67372a3bbded
Roberto Casadei <robyonrails@gmail.com>
2021-02-18 01:37:18 +0100

    More extensive analysis needed

--- build.gradle.kts
+++ build.gradle.kts
@@ -188,8 +188,9 @@ makeTest(name="hello", file = "hello_scafi", time = 100.0, vars = setOf("random"
 makeTest(name="procs", file = "test_aggregate_processes", time = 120.0, vars = setOf("random"), taskSize = 1500)
 makeTest(name="tuples", file = "spatialtuples", time = 500.0, vars = setOf("random"), taskSize = 1500)
 makeTest(name="moving", file = "spatialtuples", time = 400.0, vars = setOf("speed","random"), taskSize = 1500)
-makeTest(name="moreins", file = "spatialtuples", time = 800.0, vars = setOf("moreINsInitially","speed","random"), taskSize = 1500) // "speed"
-makeTest(name="st", file = "spatialtuples", time = 100.0, taskSize = 1500, effects="src/main/resources/spatialtuples.aes")
+makeTest(name="moreins", file = "spatialtuples", time = 800.0, vars = setOf("moreINsInitially","random"), taskSize = 1500) // "speed"
+makeTest(name="st", file = "spatialtuples", time = 100.0, taskSize = 1500, effects="src/main/resources/spatialtuples2.aes")
+makeTest(name="analysis", file = "spatialtuples", time = 800.0, vars = setOf("moreINsInitially","speed","taskFactor","retainTime","timeDistrib","random"), taskSize = 1500) // "speed"
 
 
 // defaultTasks("fatJar")
